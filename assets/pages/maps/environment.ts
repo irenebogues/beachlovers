497e1c6a42d1409f22541d08e63415ac733dba29
irenebogues <irenebogues@gmail.com>
2018-11-22 16:08:07 -0500

    initializing real time maps using mapbox

---
+++ assets/pages/maps/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiaXJlbmVib2d1ZXMiLCJhIjoiY2pvcnVpdmlhMGpvZDN1bXoza3FvZjNmcCJ9.yYaGPjMye0LuhOEXNS8lQg'
+  }
+}
